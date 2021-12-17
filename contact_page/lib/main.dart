8b2216638bac092120fa5f6c1384ada98a2fe736
Maria V <mariatua2@gmail.com>
2021-12-17 17:55:25 -0400

    buttons on contact page

--- contact_page/lib/main.dart
+++ contact_page/lib/main.dart
@@ -9,23 +9,14 @@ class ContactProfilePage extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-
-          //Adding background color to AppBar
           backgroundColor: Colors.white,
-
-          //Adding back arrow for leading property.
-          //Back arrow appears at the top-left of AppBar
-          leading: const Icon(
+          leading: Icon(
             Icons.arrow_back,
             color: Colors.black,
           ),
           actions: <Widget>[
-
-            //Adding Star icon for favorting the contact.
-            //This IconButton responds to user's tap,
-            //and prints the message.
             IconButton(
-              icon: const Icon(Icons.star_border),
+              icon: Icon(Icons.star_border),
               color: Colors.black,
               onPressed: () {
                 print("Contact is starred");
@@ -37,22 +28,162 @@ class ContactProfilePage extends StatelessWidget {
           children: <Widget>[
             Column(
               children: <Widget>[
-                //Container for adding Profile picture
                 Container(
-                  //Container is stretched horizontaly
                   width: double.infinity,
-                  //Image is fetched from url
                   child: Image.network(
                     "https://github.com/ptyagicodecamp/educative_flutter/raw/profile_1/assets/profile.jpg?raw=true",
                     height: 250,
                     fit: BoxFit.cover,
                   ),
                 ),
+                Container(
+                  height: 60,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Priyanka Tyagi",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.grey,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 8, bottom: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      buildCallButton(),
+                      buildTextButton(),
+
+                      //SOLUTION: Added Video, Email, Directions & Pay action items
+
+                      //Enabled "video" action item
+                      buildVideoCallButton(),
+
+                      //Enabled "Email" action item
+                      buildEmailButton(),
+
+                      //Enabled "Directions" action item
+                      buildDirectionsButton(),
+
+                      //Enabled "Pay" action item
+                      buildPayButton(),
+
+                    ],
+                  ),
+                ),
+                //SOLUTION: Divider widget with grey color
+                Divider(
+                  color: Colors.grey,
+                ),
               ],
             ),
           ],
         ),
       ),
+    );
+  }
+
+//Adding "Call" action item
+  Widget buildCallButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.call,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        Text("Call"),
+      ],
+    );
+  }
+
+//Adding "Text" action item
+  Widget buildTextButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.message,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        Text("Text"),
+      ],
+    );
+  }
+
+//SOLUTION: Adding "Video" action item
+  Widget buildVideoCallButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.video_call,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        Text("Video"),
+      ],
+    );
+  }
+
+//SOLUTION: Adding "Email" action item
+  Widget buildEmailButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.email,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        Text("Email"),
+      ],
+    );
+  }
+
+//SOLUTION: Adding "Directions" action item
+  Widget buildDirectionsButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.directions,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        Text("Directions"),
+      ],
+    );
+  }
+
+//SOLUTION: Adding "Pay" action item
+  Widget buildPayButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.attach_money,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        Text("Pay"),
+      ],
     );
   }
 }
