e66247d5ca3860ee12157532ddbca634ecc9e0b0
Maria V <mariatua2@gmail.com>
2021-12-17 17:14:31 -0400

    feat: hello flutter app done

--- helloflutterapp/lib/main.dart
+++ helloflutterapp/lib/main.dart
@@ -1,47 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(HelloFlutterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HelloFlutterApp extends StatefulWidget {
+  @override
+  _HelloFlutterAppState createState() => _HelloFlutterAppState();
+}
 
-  // This widget is the root of your application.
+String englishGreeting = "Hello Flutter";
+String spanishGreeting = "Hola Flutter";
+
+class _HelloFlutterAppState extends State<HelloFlutterApp> {
+  //Default greeting is in English language
+  String displayText = englishGreeting;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Hello World',
-      debugShowCheckedModeBanner: false, //remove debug banner
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Hello Flutter App"),
-          leading: Icon(Icons.home),
+          title:const Text("Hello Flutter App"),
+          leading: const Icon(Icons.home),
           actions: <Widget>[
             IconButton(
-              icon: Icon(Icons.refresh),
-              onPressed: () {},
+              icon: const Icon(Icons.refresh),
+              onPressed: () {
+                //Updating greeting language in setState method
+                setState(() {
+                  displayText = displayText == englishGreeting
+                      ? spanishGreeting
+                      : englishGreeting;
+                });
+              },
             )
           ],
         ),
-        body: const Center(
+        body: Center(
           child: Text(
-            "Hello Flutter !",
-
-            //CHALLENGE: Modify fontSize to a different value
-            style: TextStyle(fontSize: 24),
+            displayText,
+            style: const TextStyle(fontSize: 24),
           ),
         ),
       ),
